72067980c23d5398604d8e366ce0088cf55c663e
khristines238 <khristines238@gmail.com>
2025-10-23 18:21:04 +0800

    Form Design for Open Text File

--- ReadingFile/Form1.Designer.cs
+++ ReadingFile/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ReadingFile
 {
-    partial class Form1
+    partial class FrmOpenTextFile
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lvShowText = new System.Windows.Forms.ListView();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lvShowText
+            // 
+            this.lvShowText.HideSelection = false;
+            this.lvShowText.Location = new System.Drawing.Point(12, 22);
+            this.lvShowText.Name = "lvShowText";
+            this.lvShowText.Size = new System.Drawing.Size(579, 239);
+            this.lvShowText.TabIndex = 0;
+            this.lvShowText.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnOpen.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnOpen.Location = new System.Drawing.Point(265, 281);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = false;
+            // 
+            // FrmOpenTextFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(603, 325);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.lvShowText);
+            this.Name = "FrmOpenTextFile";
+            this.Text = "FrmOpenTextFile";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView lvShowText;
+        private System.Windows.Forms.Button btnOpen;
     }
 }
 
--- ReadingFile/Form1.cs
+++ ReadingFile/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ReadingFile
 {
-    public partial class Form1 : Form
+    public partial class FrmOpenTextFile : Form
     {
-        public Form1()
+        public FrmOpenTextFile()
         {
             InitializeComponent();
         }
